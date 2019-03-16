62a0f13f56210527d78724fe096ea9cdd3972aa8
a298030171 <298030171@qq.com>
2019-03-16 18:33:43 +0800

    complete

--- public/template/js/car-main.js
+++ public/template/js/car-main.js
@@ -1,7 +1,7 @@
 import "../libs/jquery-1.12.4.js";
 import "../libs/jquery.cookie.js";
 
-import {ShoppingCar} from "./shoppingcar.JS"
+import {ShoppingCar} from "./shoppingcar.js"
 import { Islogin } from "./Islogin.js";
 import { Tocar } from "./tocar.js";
 
